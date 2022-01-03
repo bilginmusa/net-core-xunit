ebe73781c1b198023350b138ae1fbb605f670e82
Musa Bilgin <musa_bilgin_55@hotmail.com>
2022-01-03 13:46:14 +0300

    initial commit

---
+++ UnitTestExample.Test/Base.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using UnitTestExample.App.Models;
+using UnitTestExample.App.Repository;
+
+namespace UnitTestExample.Test
+{
+    public class Base<TEntitiy> where TEntitiy : class
+    {
+
+        private readonly IServiceProvider serviceProvider;
+
+        internal readonly IRepository<TEntitiy> _repo;
+
+        public Base()
+        {
+            var configuration = new ConfigurationBuilder()
+           .AddJsonFile("appsettings.json", optional: true)
+           .AddEnvironmentVariables()
+           .Build();
+
+            var services = new ServiceCollection();
+
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+
+            services.AddDbContext<UnitTestDbContext>(options =>
+            {
+                options.UseSqlServer(configuration["SqlConn"]);
+            });
+
+            serviceProvider = services.BuildServiceProvider();
+
+            _repo = serviceProvider.GetService<IRepository<TEntitiy>>();
+        }
+    }
+}
---
+++ UnitTestExample.Test/ProductControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnitTestExample.App.Models;
+using Xunit;
+
+namespace UnitTestExample.Test
+{
+    public class ProductControllerTest : Base<Product>
+    {
+        [Fact]
+        public async Task Index_GetRepoExecutes_ReturnAllData()
+        {
+            var result = await _repo.GetAll();
+            Assert.NotNull(result);
+        }
+    }
+}
+
+
